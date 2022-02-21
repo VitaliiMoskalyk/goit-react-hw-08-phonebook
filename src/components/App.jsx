0d6d726f7f30d553e359997c249116cca39a5197
Vitalik <vitalikmoskalik@gmail.com>
2022-02-21 11:38:18 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ export const App = () => {
   return (
     <>
       <Routes>
-//         {<Route index element={<HomeView />} />}
+
 
         <Route index element={<UserView />} />
         <Route path="user" element={<HomeView />} />
