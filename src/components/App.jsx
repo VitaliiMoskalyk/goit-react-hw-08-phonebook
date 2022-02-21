2c6b3df87eaf133363d30c87fca25a49efdcd202
Vitalik <vitalikmoskalik@gmail.com>
2022-02-21 11:23:13 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
-// import { HomeView } from 'views/HomeView';
+import { HomeView } from 'views/HomeView';
 import { UserView } from 'views/UserView';
 import { FormView } from 'views/FormView';
 export const App = () => {
   return (
     <>
       <Routes>
-        {/* <Route index element={<HomeView />} /> */}
+//         {<Route index element={<HomeView />} />}
 
         <Route index element={<UserView />} />
-//         <Route path="user" element={<UserView />} />
+        <Route path="user" element={<HomeView />} />
         <Route path="user/add" element={<FormView />} />
       </Routes>
     </>
