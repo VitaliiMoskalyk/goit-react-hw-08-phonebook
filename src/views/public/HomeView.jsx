030c72f439025b257050d7f6f4136795b9212e0b
VitaliiMoskalyk <vitalikmoskalik@gmail.com>
2022-03-12 21:05:03 +0200

    Update HomeView.jsx

--- src/views/public/HomeView.jsx
+++ src/views/public/HomeView.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ContactList from 'components/ContactList';
 import toast, { Toaster } from 'react-hot-toast';
